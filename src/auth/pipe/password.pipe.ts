630d557da4788a107d69adb143c362617fc69b83
Seongmin Oh <saogig0503@naver.com>
2026-02-08 09:34:30 +0900

    MinLengthPipe, MaxLengthPipe 구현 및 적용

--- src/auth/pipe/password.pipe.ts
+++ src/auth/pipe/password.pipe.ts
@@ -1,10 +1,25 @@
 import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
 
 @Injectable()
-export class PasswordPipe implements PipeTransform {
+export class MaxLengthPipe implements PipeTransform {
+  constructor(private readonly length: number) {}
+
   transform(value: any) {
-    if (value.toString().length > 8) {
-      throw new BadRequestException('비밀번호는 8자 이하로 입력해주세요.');
+    if (value.toString().length > this.length) {
+      throw new BadRequestException(`최대 길이는 ${this.length}입니다.`);
+    }
+
+    return value.toString();
+  }
+}
+
+@Injectable()
+export class MinLengthPipe implements PipeTransform {
+  constructor(private readonly length: number) {}
+
+  transform(value: any) {
+    if (value.toString().length < this.length) {
+      throw new BadRequestException(`최소 길이는 ${this.length}입니다.`);
     }
 
     return value.toString();
