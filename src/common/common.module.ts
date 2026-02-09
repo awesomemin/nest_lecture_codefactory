796b20b925dc08c0c2a75e752a10a0d10362c3ac
Seongmin Oh <saogig0503@naver.com>
2026-02-09 09:04:14 +0900

    Common 모듈에서 공통 이미지 업로드 엔드포인트 생성

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,10 +1,45 @@
-import { Module } from '@nestjs/common';
+import { BadRequestException, Module } from '@nestjs/common';
 import { CommonService } from './common.service';
 import { CommonController } from './common.controller';
+import { MulterModule } from '@nestjs/platform-express';
+import * as multer from 'multer';
+import { extname } from 'path';
+import { TEMP_FOLDER_PATH } from './const/path.const';
+import { v4 as uuid } from 'uuid';
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   controllers: [CommonController],
   providers: [CommonService],
   exports: [CommonService],
+  imports: [
+    MulterModule.register({
+      limits: {
+        fileSize: 10000000,
+      },
+      fileFilter: (req, file, cb) => {
+        const ext = extname(file.originalname);
+        if (ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png') {
+          return cb(
+            new BadRequestException('jpg/jpeg/png 파일만 업로드 가능합니다.'),
+            false,
+          );
+        }
+
+        return cb(null, true);
+      },
+      storage: multer.diskStorage({
+        destination: function (req, res, cb) {
+          cb(null, TEMP_FOLDER_PATH);
+        },
+        filename: function (req, file, cb) {
+          cb(null, `${uuid()}${extname(file.originalname)}`);
+        },
+      }),
+    }),
+    AuthModule,
+    UsersModule,
+  ],
 })
 export class CommonModule {}
